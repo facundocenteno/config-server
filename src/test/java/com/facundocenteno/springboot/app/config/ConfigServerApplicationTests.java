61d3253889f11deace2ad8f25844855bc8ac25e4
facundocenteno <facucenteno1994@gmail.com>
2023-06-04 18:20:25 -0300

    first commit

---
+++ src/test/java/com/facundocenteno/springboot/app/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facundocenteno.springboot.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
